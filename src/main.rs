015492d2843e012f9b6fd6c9ccb8ef10e687a1e4
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 20:24:41 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use structopt::StructOpt;
-use exitfailure::ExitFailure;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
-#[derive(StructOpt)]
-struct inputs {
-    /// The commit message
-    message: String,
-}
-fn main() -> Result<(), ExitFailure> {
-    let message = inputs::from_args();
-    let child = Command::new("/usr/bin/sh")
-                .args(&["-c", "git add ."])
-                .stderr(std::process::Stdio::null()) // don't care about stderr
-                .stdout(std::process::Stdio::piped()) // set up stdout so we can read it
-                .stdin(std::process::Stdio::piped()) // set up stdin so we can write on it
-                .spawn().expect("Could not run the command"); // finally run the command
-    Ok(())
+fn main() {
+    let mut child = Command::new("/usr/bin/sh");
+    if let Err(e) = child
+        .arg("-c")
+        .arg("git add .")
+        .arg("git commit -S -m 'from my PC'")
+        .arg("git push")
+        .stderr(Stdio::piped())
+        .output()
+    {
+        println!("{}", e.to_string());
+    }
 }
