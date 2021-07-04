661c2ac0aab62baaa80171ea32d2b096072e5a54
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 21:20:36 +0530

    from my PC

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ fn main() {
     let mut child = Command::new("/usr/bin/sh");
     child
     .args(&["-c", "git add . && git commit -S -m 'from my PC' && git push"])
-    .stderr(std::process::Stdio::null()) // don't care about stderr
     .stdout(std::process::Stdio::piped()) // set up stdout so we can read it
     .stdin(std::process::Stdio::piped()) // set up stdin so we can write on it
     .spawn().expect("Could not run the command"); // finally run the command
