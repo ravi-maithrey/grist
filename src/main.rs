4c990130142bd16b5c1c1e5c0337f182cf011a74
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 21:28:46 +0530

    from my PC

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     child
     .args(&["-c", "git add . && git commit -S -m 'from my PC' && git push"])// set up stdin so we can write on it
     .status().expect("Could not run the command"); // finally run the command
-    println!("process finished with: {:?}", child);
+    
 }
