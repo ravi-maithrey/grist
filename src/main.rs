94259683fed03558e71e13f55cfefb3db8f7e195
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 21:25:34 +0530

    from my PC

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let mut child = Command::new("/usr/bin/sh");
     child
     .args(&["-c", "git add . && git commit -S -m 'from my PC' && git push"])// set up stdin so we can write on it
-    .spawn().expect("Could not run the command"); // finally run the command
+    .output().expect("Could not run the command"); // finally run the command
     
 }
