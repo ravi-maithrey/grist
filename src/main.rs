b3d2f9322e7d50a405c82801112f2e4f420b6e0a
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 21:21:24 +0530

    from my PC

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::process::Command;
 fn main() {
     let mut child = Command::new("/usr/bin/sh");
     child
-    .args(&["-c", "git add . && git commit -S -m 'from my PC' && git push"])
-    .stdout(std::process::Stdio::piped()) // set up stdout so we can read it
-    .stdin(std::process::Stdio::piped()) // set up stdin so we can write on it
+    .args(&["-c", "git add . && git commit -S -m 'from my PC' && git push"])// set up stdin so we can write on it
     .spawn().expect("Could not run the command"); // finally run the command
     
 }
