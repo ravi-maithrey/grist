aefc50d7b0cd51bdaa89cc5a5a4f30eb2c07dcea
ravi maithrey <maitrey.ind@gmail.com>
2021-07-04 20:27:57 +0530

    final trial for today

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use std::process::{Command, Stdio};
+use std::process::Command;
 
 fn main() {
     let mut child = Command::new("/usr/bin/sh");
     if let Err(e) = child
         .arg("-c")
-        .arg("git add .")
-        .arg("git commit -S -m 'from my PC'")
-        .arg("git push")
-        .stderr(Stdio::piped())
+        .arg("git add . && git push -m 'from my PC' && git push")
         .output()
     {
         println!("{}", e.to_string());
