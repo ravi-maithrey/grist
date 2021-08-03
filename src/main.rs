4229f88040b6245071050080d0a0cece5d4b592f
ravi maithrey <maitrey.ind@gmail.com>
2021-08-03 08:43:57 +0530

    Opt { message: "test" }

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ fn main() {
     let message = format!("{:?}", option);
     let mut child = Command::new("/usr/bin/sh");
     // let command = "git add . && git commit -S -m '" + message + "' && git push"; 
-    let command = format!("git add . && git commit ")
+    let command = format!("git add . && git commit -S -m '{}' && git push", message);
     child
         .args(&[
             "-c",
-            "git add . && git commit -S -m 'from my PC' && git push",
+            &command,
         ]) // set up stdin so we can write on it
         .status()
         .expect("Could not run the command"); // finally run the command
