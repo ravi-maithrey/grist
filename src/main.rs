a093c84dd0bca5be4d7e1a67dd17c44a04a6497b
ravi maithrey <maitrey.ind@gmail.com>
2021-08-03 08:52:58 +0530

    some cleanup and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::process::Command;
 use clap::Clap;
+use std::process::Command;
 
 #[derive(Clap)]
-#[clap(version="1.1", author="Ravi Maithrey <maitrey.ind@gmail.com>")]
+#[clap(version = "1.1", author = "Ravi Maithrey <maitrey.ind@gmail.com>")]
 struct Opts {
     // The message to be passed in while commiting to git
     /// The message to be passed in while commiting to git
@@ -13,13 +13,10 @@ fn main() {
     let option = Opts::parse();
     let message = option.message;
     let mut child = Command::new("/usr/bin/sh");
-    // let command = "git add . && git commit -S -m '" + message + "' && git push"; 
+    // let command = "git add . && git commit -S -m '" + message + "' && git push";
     let command = format!("git add . && git commit -S -m '{}' && git push", message);
     child
-        .args(&[
-            "-c",
-            &command,
-        ]) // set up stdin so we can write on it
+        .args(&["-c", &command]) // set up stdin so we can write on it
         .status()
         .expect("Could not run the command"); // finally run the command
 }
